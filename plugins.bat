2682a0a27f7dbb164c2c541e8819fd8a8a3bf63d
Carles Ameller <carles.ameller@laviniainteractiva.com>
2016-12-09 08:58:59 +0100

    Automated task to install all plugins/vendors

---
+++ plugins.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+echo "[INFO] -------------------------------------------------------------"
+echo "[INFO] INSTALLING PLUGINS"
+echo "[INFO] -------------------------------------------------------------"
+
+echo "[INFO] Installing MODAL(VodkaBears/Remodal)"
+call npm install remodal
+
+echo "[INFO] Installing SLIDER(kenwheeler/slick)"
+call npm install slick-carousel
+
+echo "[INFO] Installing CIRCULAR PROGRESSBAR (aterrien/jQuery-Knob)"
+call npm install aterrien/jQuery-Knob
+
+echo "[INFO] Installing CUSTOM SCROLLBARS (malihu/malihu-custom-scrollbar-plugin)"
+call npm install malihu-custom-scrollbar-plugin
+
+echo "[INFO] Installing CUSTOM SELECTS (select2/select2)"
+call npm install select2/select2
+
+echo "[INFO] Installing RANGESLIDER (IonDen/ion.rangeSlider)"
+call npm install ion-rangeslider
+
+echo "[INFO] Installing TOOLTIPS (iamceege/tooltipster)"
+call npm install iamceege/tooltipster
+
+echo "[INFO] Installing TRIGGER IN VIEWPORT (silvestreh/onScreen)"
+call npm install onscreen
+
+echo "[INFO] Installing MASONRY (desandro/masonry)"
+call npm install masonry-layout
